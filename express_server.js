458f5f41a59b4ef46fc212f004fbefa4189142f1
Andrew Matthews <andymat1964@gmail.com>
2020-12-07 21:18:20 +0000

    added templates

--- express_server.js
+++ express_server.js
@@ -1,24 +1,31 @@
-const express = require("express");
-const app = express();
-const PORT = 8080; // default port 8080
+// load the things we need
+var express = require('express');
+var app = express();
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+// set the view engine to ejs
+app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    var mascots = [
+        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+        { name: 'Tux', organization: "Linux", birth_year: 1996},
+        { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+    ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
+
+    res.render('pages/index', {
+        mascots: mascots,
+        tagline: tagline
+    });
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+app.listen(8080);
+console.log('8080 is the magic port');
