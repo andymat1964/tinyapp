8b5eed84ef6ca010e793ac82e222fd902d99a641
Andrew Matthews <andymat1964@gmail.com>
2020-12-08 01:29:33 +0000

    URL shortening 1

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 // load the things we need
 var express = require('express');
 var app = express();
+const bodyParser = require("body-parser");
+  app.use(bodyParser.urlencoded({extended: true}));
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -12,6 +14,10 @@ const urlDatabase = {
     "9sm5xK": "http://www.google.com"
   };
   
+  function generateRandomString() {
+    return Math.random().toString(36).substring(2,8);
+  }
+
 
 // index page
 app.get('/', function(req, res) {
@@ -38,11 +44,20 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
   });
 
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
   app.get("/urls/:shortURL", (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
     res.render("urls_show", templateVars);
   });
 
+
+  app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
 
 app.listen(8080);
 console.log('8080 is the magic port');
