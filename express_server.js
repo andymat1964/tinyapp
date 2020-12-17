2582072cb66576a33884ee478796c0ecdcfa6b21
Andrew Matthews <andymat1964@gmail.com>
2020-12-17 12:42:41 +0000

    put user & url datatbase back in express_server.js

--- express_server.js
+++ express_server.js
@@ -20,6 +20,22 @@ app.set('view engine', 'ejs');
 
 
 
+const urlDatabase = {
+  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "aJ48lW" },
+  i3BoGr: { longURL: "https://www.google.ca", userID: "12s1vd" }
+};
+
+
+const users = { 
+  "12s1vd": {
+    id: "12s1vd", 
+    email: "user@example.com", 
+    password: '$2b$10$zp8yf/f8NiVRxNzxzHHgFOzKKkuNFeeUkdnWfQJRtsiJJ0bRx1Bjy'
+  },
+};
+
+
+
 
 // Displays the index page
 app.get('/', function(req, res) {
